cab0f0cab8c4d7400613f18973c82be92fab48d4
Carlos Delgado <cdelgaga@nttdata.com>
2023-09-20 11:38:32 -0500

    first commit

---
+++ projectEuler.py
@@ -0,0 +1,42 @@
+def multiplos35():
+    numero_entero = int(input("Por favor, ingrese un número entero: "))
+    sum=0
+    for i in range(numero_entero):
+        if i % 3 == 0 or i % 5 == 0:
+            sum  = sum + i
+        else:
+            print("")
+    return print(sum)
+
+multiplos35()
+
+'''#Funcion para calcular serie de fibonacci   
+def fib(n):
+    if n < 2:
+        return n 
+    else:
+        return fib(n-1)+fib(n-2)
+
+#Usando serie de fibonacci en un rango de 10
+
+sum = 2
+a = 1
+b = 2
+c = 0
+
+while c <= 4000000:
+    c = a + b
+    if c%2 == 0:
+        sum += c
+    a,b = b,c
+    print(a,b)'''
+
+
+
+
+
+
+    
+    
+    
+    
